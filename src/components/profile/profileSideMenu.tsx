3b0186d6de44889d8324bf2282e8150e05a7855a
Erfan <erfansormi97@gmail.com>
2023-03-28 12:26:40 +0330

    at profile page, header with user name and email added

--- src/components/profile/profileSideMenu.tsx
+++ src/components/profile/profileSideMenu.tsx
@@ -7,19 +7,44 @@ import styles from "./profile.module.css";
 
 // icons
 import { BsBoxArrowRight } from 'react-icons/bs'
+import { FaUserCircle } from 'react-icons/fa';
+import { AiOutlineEdit } from 'react-icons/ai';
 
 // data
 import { profileSideMenuData } from './profileData'
 
+// context
+import { useUserContext } from '../../context/userContext';
+
 const ProfileSideMenu = () => {
     const router = useRouter();
+
+    // user context
+    const { user } = useUserContext();
 
     return (
         <div className='capitalize text-gray-900'>
 
             {/* header */}
-            <div>
-
+            <div className='p-5 flex items-center gap-x-4'>
+                <div>
+                    <FaUserCircle className='text-gray-300 text-[3.3rem] leading-5' />
+                </div>
+                <div className='flex items-center justify-between w-full'>
+                    <div className='flex flex-col gap-y-0.5'>
+                        <span className='text-gray-700 text-lg font-bold'>
+                            {user?.first_name} {user?.last_name}
+                        </span>
+                        <span className='text-gray-400 text-sm normal-case'>
+                            {user?.email}
+                        </span>
+                    </div>
+                    <div>
+                        <Link href={"/profile/personal-info"}>
+                            <AiOutlineEdit className='text-2xl text-cyan-500' />
+                        </Link>
+                    </div>
+                </div>
             </div>
 
             {/* items */}
@@ -31,7 +56,7 @@ const ProfileSideMenu = () => {
                     >
                         {
                             router.pathname === item.link &&
-                            <span className={`${styles.menu_item_selected} before:bg-rose-500`}>
+                            <span className={`${styles.menu_item_selected} before:bg-rose-500 before:content-['']`}>
                             </span>
                         }
                         <Link
