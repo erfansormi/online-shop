7efc498935c5a605d4c1d9fabb3dcf092bd38f60
Erfan <erfansormi97@gmail.com>
2023-04-01 12:09:54 +0330

    activities page in profile created (it's have some component like recent visits or favorites list)

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,13 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+// contexts
+import { useUserContext } from '../../context/userContext'
+
+// types
+import { Product } from '../../types/product/productTypes'
+
+// axios
+import { axiosInstance } from '../../functions/axiosInstance'
 
 // components
+import ProfileContentContainer from '../../components/profile/profileContentContainer'
+import TabContentSlider from '../../components/utils/sliders/tabContentSlider'
 import ProfileContainer from '../../components/profile/profileContainer'
+import ProductSkeletonSlider from '../../components/utils/sliders/productSkeletonSlider'
+import TabContentTitle from '../../components/data_display/tabContentTitle'
 
 const Profile = () => {
+    const { user } = useUserContext();
+
+    // states
+    const [recentVisits, setRecentVisits] = useState<Product[] | null>(null);
+
+    useEffect(() => {
+        axiosInstance.get("/api/v1/users/recent-visits")
+            .then((res) => {
+                setRecentVisits(res.data.recentVisits);
+            })
+    }, [])
+
     return (
-        <ProfileContainer>
-            Activities
-        </ProfileContainer>
+        <>
+            {
+                user ?
+                    <>
+                        <ProfileContainer>
+                            {/* recent products visits */}
+                            <ProfileContentContainer>
+                                {
+                                    // skeleton loading
+                                    recentVisits === null ?
+                                        <ProductSkeletonSlider title='recent visits' /> :
+
+                                        // data
+                                        <TabContentSlider data={user.activities.recent_visits} title="recent visits" />
+                                }
+                            </ProfileContentContainer>
+
+                            {/* favorites list */}
+                            <ProfileContentContainer>
+                                <TabContentSlider data={user.favorites_list} title="favorites list" />
+                            </ProfileContentContainer>
+                        </ProfileContainer>
+                    </>
+                    :
+                    //  skeleton loading 
+                    <ProfileContainer >
+                        <ProfileContentContainer>
+                            <ProductSkeletonSlider title='recent visits' />
+                        </ProfileContentContainer>
+
+                        <ProfileContentContainer>
+                            <ProductSkeletonSlider title='favorites list' />
+                        </ProfileContentContainer>
+                    </ProfileContainer>
+            }
+        </>
     )
 }
 
