457c99a279318b3523661e1d6e1fa540f455eb7a
Erfan <erfansormi97@gmail.com>
2023-04-07 13:57:41 +0330

    Update axiosInstance.ts

--- src/functions/axiosInstance.ts
+++ src/functions/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     withCredentials: true,
-    baseURL: process.env.DEV_URL,
+    baseURL: process.env.URL,
     headers: {
         "Content-Type": "application/json",
     },
