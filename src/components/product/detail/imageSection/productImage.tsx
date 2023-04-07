f2181989f4fc3a4e93b94ed29134227ba8f98120
Erfan <erfansormi97@gmail.com>
2023-04-07 13:55:55 +0330

    top left buttons responsived in product detail

--- src/components/product/detail/imageSection/productImage.tsx
+++ src/components/product/detail/imageSection/productImage.tsx
@@ -3,91 +3,19 @@ import Image from 'next/image';
 
 // contexts
 import { useProductContext } from '../../productContainer';
-import { useUserContext } from '../../../../context/userContext';
 
-// react toastify
-import { toastify } from '../../../utils/toastify/toastifyFunc';
-
-// icons
-import { FiHeart } from 'react-icons/fi';
-import { FaHeart } from 'react-icons/fa';
-import { BsShareFill } from 'react-icons/bs';
-
-// axios
-import { axiosInstance } from '../../../../functions/axiosInstance';
-import { Tooltip } from '@mui/material';
+// components
+import TopLeftButtons from './topLeftButtons';
 
 const ProductImage = () => {
-    // contexts
-    const { user, setUser } = useUserContext();
     const { productInfo } = useProductContext();
     const { product } = productInfo;
 
-    // like product handle click
-    const handleLikeProduct = async () => {
-        await axiosInstance.post(`/api/v1/users/${product._id}/add-to-favorites`)
-            .then((res) => {
-                if (user) {
-                    setUser({
-                        ...user,
-                        favorites_list: res.data.favorites_list
-                    })
-                }
-            })
-            .catch((err) => {
-                toastify("please login first!", "error")
-            })
-    }
-
-    // if product there is in favorites list returned true else returned false
-    const isThereInFavorites = () => {
-        if (!user) {
-            return false;
-        }
-
-        if (user.favorites_list.find(item => item._id == product._id)) {
-            return true
-        }
-        return false
-    }
-
-    // handle copy product link
-    const handleCopyLink = () => {
-        if (typeof window !== "undefined") {
-            window.navigator.clipboard.writeText(window.location.href);
-            toastify("product address copied!", "info")
-        }
-    }
-
     return (
-        <div className='flex w-full gap-x-3'>
+        <div className='flex lg:flex-row flex-col w-full gap-x-3 gap-y-1'>
 
             {/* left buttons */}
-            <div className='text-slate-700 flex flex-col gap-y-3 text-2xl'>
-
-                {/* like product */}
-                <span onClick={handleLikeProduct} className="cursor-pointer">
-                    {
-                        isThereInFavorites() ?
-                            <Tooltip title="Remove From Favorites">
-                                <div>
-                                    <FaHeart className=' text-red-500' />
-                                </div>
-                            </Tooltip> :
-                            <Tooltip title="Add To Favorites">
-                                <div>
-                                    <FiHeart />
-                                </div>
-                            </Tooltip>
-
-                    }
-                </span>
-
-                {/* share product */}
-                <span className='cursor-pointer' onClick={handleCopyLink} >
-                    <BsShareFill />
-                </span>
-            </div>
+            <TopLeftButtons />
 
             {/* image */}
             <div className='overflow-hidden py-8 border border-solid border-gray-200 rounded-lg select-none w-full'>
---
+++ src/components/product/detail/imageSection/topLeftButtons.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import { axiosInstance } from '../../../../functions/axiosInstance';
+
+// icons
+import { FiHeart } from 'react-icons/fi';
+import { FaHeart } from 'react-icons/fa';
+import { BsShareFill } from 'react-icons/bs';
+
+// contexts
+import { useUserContext } from '../../../../context/userContext';
+import { useProductContext } from '../../productContainer';
+
+// toast
+import { toastify } from '../../../utils/toastify/toastifyFunc';
+
+// components
+import { Tooltip } from '@mui/material';
+
+const TopLeftButtons = () => {
+    const { user, setUser } = useUserContext();
+    const { productInfo } = useProductContext();
+    const { product } = productInfo;
+
+    // like product handle click
+    const handleLikeProduct = async () => {
+        await axiosInstance.post(`/api/v1/users/${product._id}/add-to-favorites`)
+            .then((res) => {
+                if (user) {
+                    setUser({
+                        ...user,
+                        favorites_list: res.data.favorites_list
+                    })
+                }
+            })
+            .catch((err) => {
+                toastify("please login first!", "error")
+            })
+    }
+
+    // if product there is in favorites list returned true else returned false
+    const isThereInFavorites = () => {
+        if (!user) {
+            return false;
+        }
+
+        if (user.favorites_list.find(item => item._id == product._id)) {
+            return true
+        }
+        return false
+    }
+
+    // handle copy product link
+    const handleCopyLink = () => {
+        if (typeof window !== "undefined") {
+            window.navigator.clipboard.writeText(window.location.href);
+            toastify("product address copied!", "info")
+        }
+    }
+
+    return (
+        <div className='text-slate-700 flex lg:flex-col gap-y-3 gap-x-5 text-2xl'>
+
+            {/* like product */}
+            <span onClick={handleLikeProduct} className="cursor-pointer">
+                {
+                    isThereInFavorites() ?
+                        <Tooltip title="Remove From Favorites">
+                            <div>
+                                <FaHeart className=' text-red-500' />
+                            </div>
+                        </Tooltip> :
+                        <Tooltip title="Add To Favorites">
+                            <div>
+                                <FiHeart />
+                            </div>
+                        </Tooltip>
+
+                }
+            </span>
+
+            {/* share product */}
+            <span className='cursor-pointer' onClick={handleCopyLink} >
+                <BsShareFill />
+            </span>
+        </div>
+    )
+}
+
+export default TopLeftButtons;
