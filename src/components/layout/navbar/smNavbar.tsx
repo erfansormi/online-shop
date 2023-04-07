4cf6f51200455af0240e4fa0f30e49a7b3af9a91
Erfan <erfansormi97@gmail.com>
2023-04-07 12:49:17 +0330

    some bug in small navbar fixed and some style in small navbar changed (like navbar height, skeleton loading size and etc)

--- src/components/layout/navbar/smNavbar.tsx
+++ src/components/layout/navbar/smNavbar.tsx
@@ -15,23 +15,23 @@ const SmNavbar = () => {
     const router = useRouter();
 
     // user context
-    const { user } = useUserContext();
+    const { user, loading } = useUserContext();
 
     return (
         <nav
             className='md:hidden bg-white shadow-md z-[1000] fixed bottom-0 right-0 left-0'
-            style={{ height: "var(--navbar-height)" }}
+            style={{ height: "var(--sm-navbar-height)" }}
         >
             <div className='w-full h-full flex justify-around items-center'>
                 {
-                    user ?
+                    !loading ?
                         smNavData(user).map((item, index) =>
                             <div key={index * 37}>
                                 <Link
                                     href={item.link}
                                     className={`h-full flex flex-col ${item.link !== "/" && router.pathname.includes(item.link) ? "text-rose-500" : item.link === "/" && router.pathname === "/" ? "text-rose-500" : "text-gray-700"} w-16 justify-center items-center`}
                                 >
-                                    <span className='text-3xl'>
+                                    <span className='text-2xl'>
                                         {item.icon}
                                     </span>
                                     <span className='capitalize text-sm'>
@@ -42,7 +42,7 @@ const SmNavbar = () => {
                         ) :
                         [...Array(4)].map((item, index) =>
                             <div key={index * 68} className="h-full flex items-center">
-                                <Skeleton width={50} height={80} />
+                                <Skeleton width={35} height={35} className='scale-100' />
                             </div>
                         )
                 }
