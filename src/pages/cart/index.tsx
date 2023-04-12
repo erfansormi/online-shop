8308c1a05ae2fa277be643a646fb5f341c9ee03c
Erfan <erfansormi97@gmail.com>
2023-04-12 14:37:48 +0330

    created skeleton loading in cart page and some elements converted to component

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -2,13 +2,14 @@ import React, { useEffect } from 'react'
 import { useRouter } from "next/router";
 
 // contexts
-import { useUserContext } from '../../context/userContext'
+import { useUserContext } from '../../context/userContext';
 
 // components
 import Layout from '../../components/layout/layout';
 import TabContentTitle from '../../components/data_display/tabContentTitle';
 import CartProducts from '../../components/cart/products';
-import { Button } from '@mui/material';
+import CartCheckoutBox from '../../components/cart/checkoutBox';
+import CartSkeletonPage from '../../components/cart/skeletonPage';
 
 const Cart = () => {
     const { user, loading } = useUserContext();
@@ -21,82 +22,49 @@ const Cart = () => {
     }, [user, loading])
 
     return (
-        <>
+        <Layout className='mt-8'>
             {
                 !user ?
-                    <div className='min-h-[80vh]'>
-                    </div>
-                    :
-                    <Layout className='mt-8'>
-                        <div className='flex flex-col'>
+                    <CartSkeletonPage /> :
+                    <div className='flex flex-col'>
 
-                            {/* title */}
-                            <div className='mb-10'>
-                                <TabContentTitle title='cart' />
-                            </div>
-
-                            <div className='flex flex-col md:flex-row gap-4'>
-
-                                {/* products section */}
-                                <div className='lg:w-[75%] md:w-[65%] w-full py-4 border-gray-200 border-solid border rounded-lg flex flex-col gap-y-6'>
-
-                                    {/* title */}
-                                    <div className='flex flex-col px-6 pt-2'>
-                                        <div className='flex flex-col gap-y-2'>
-                                            <h5 className='text-gray-800'>
-                                                your cart
-                                            </h5>
-                                            <span className='text-gray-500 text-xs'>
-                                                {user.cart.products_counts} products
-                                            </span>
-                                        </div>
-                                    </div>
-
-                                    {/* products */}
-                                    <div>
-                                        {user.cart.products.map(item =>
-                                            <CartProducts {...item} key={item.product.slug} />
-                                        )}
-                                    </div>
-                                </div>
-
-                                {/* checkout info */}
-                                <div className='lg:w-[25%] md:w-[35%] w-full text-gray-800 font-medium md:sticky top-24 capitalize px-6 py-8 h-fit border-gray-200 border-solid border rounded-lg flex flex-col gap-y-6'>
-
-                                    {/* products prices */}
-                                    <div className='flex items-center justify-between text-gray-600'>
-                                        <span>price of products ({user.cart.products_counts})</span>
-                                        <span>${user.cart.products_prices}</span>
-                                    </div>
-
-                                    {/* total cart prices */}
-                                    <div className='flex items-center justify-between'>
-                                        <span>total cart</span>
-                                        <span>${user.cart.total_prices_cart}</span>
-                                    </div>
-
-                                    {/* total profit */}
-                                    {
-                                        user.cart.total_profit ?
-                                            <div className='flex items-center justify-between text-red-500'>
-                                                <span>your profit</span>
-                                                <span>${user.cart.total_profit} ({user.cart.total_profit_percentage}%)</span>
-                                            </div> :
-                                            null
-                                    }
-
-                                    {/* order registration */}
-                                    <div>
-                                        <Button variant="contained" fullWidth>
-                                            order registration
-                                        </Button>
-                                    </div>
-                                </div>
-                            </div>
+                        {/* title */}
+                        <div className='mb-10'>
+                            <TabContentTitle title='cart' />
                         </div>
-                    </Layout>
+
+                        <div className='flex flex-col md:flex-row gap-4'>
+
+                            {/* products section */}
+                            <div className='lg:w-[75%] md:w-[65%] w-full py-4 border-gray-200 border-solid border rounded-lg flex flex-col gap-y-6'>
+
+                                {/* title */}
+                                <div className='flex flex-col px-6 pt-2'>
+                                    <div className='flex flex-col gap-y-2'>
+                                        <h5 className='text-gray-800'>
+                                            your cart
+                                        </h5>
+                                        <span className='text-gray-500 text-xs'>
+                                            {user.cart.products_counts} products
+                                        </span>
+                                    </div>
+                                </div>
+
+                                {/* products */}
+                                <div>
+                                    {user.cart.products.map(item =>
+                                        <CartProducts {...item} key={item.product.slug} />
+                                    )}
+                                </div>
+                            </div>
+
+                            {/* checkout info */}
+                            <CartCheckoutBox />
+
+                        </div>
+                    </div>
             }
-        </>
+        </Layout>
     )
 }
 
