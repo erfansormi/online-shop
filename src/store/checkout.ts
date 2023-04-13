997ed8faa76bfd481f55f683d63f2caa1835605c
Erfan <erfansormi97@gmail.com>
2023-04-13 18:21:21 +0330

    updated checkout store

--- src/store/checkout.ts
+++ src/store/checkout.ts
@@ -1,16 +1,24 @@
 import { create } from "zustand";
+import { DateObject } from "react-multi-date-picker";
 
 interface UseCheckout {
     deliveryAddressModal: boolean,
     selectedAddressId: string,
+    deliveryHour: string,
+    deliveryDate: Date,
+
     setDeliveryAddressModal: (isOpen: boolean) => void,
     setSelectedAddressId: (addressId: string) => void,
+    setDeliveryHour: (hour: string) => void,
+    setDeliveryDate: (date: Date) => void
 }
 
 const useCheckout = create<UseCheckout>((set, get) => ({
     // initial values
     deliveryAddressModal: false,
     selectedAddressId: "",
+    deliveryHour: "",
+    deliveryDate: new Date(new DateObject().add(2, "day").format()),
 
     // functions
     setDeliveryAddressModal: (isOpen: boolean) => set(() => ({
@@ -18,6 +26,12 @@ const useCheckout = create<UseCheckout>((set, get) => ({
     })),
     setSelectedAddressId: (addressId: string) => set(() => ({
         selectedAddressId: addressId
+    })),
+    setDeliveryHour: (hour: string) => set(() => ({
+        deliveryHour: hour
+    })),
+    setDeliveryDate: (date) => set(() => ({
+        deliveryDate: date
     }))
 }))
 
