2360f8449dffdd2c7a1cc490c0cce7da654b14e5
Erfan <erfansormi97@gmail.com>
2023-04-13 14:43:01 +0330

    top sticky position changed

--- src/components/checkout/shipping/checkoutBox.tsx
+++ src/components/checkout/shipping/checkoutBox.tsx
@@ -8,7 +8,7 @@ const CheckoutBox = () => {
     const { user } = useUserContext();
 
     return (
-        <div className='sticky top-12 p-5 capitalize border-gray-200 border-solid rounded-lg border'>
+        <div className='sticky top-24 p-5 capitalize border-gray-200 border-solid rounded-lg border'>
             <div className='px-2 py-4 flex items-center justify-between text-gray-700 border-b-gray-200'>
                 <span>
                     price of products ({user?.cart.products_counts})
