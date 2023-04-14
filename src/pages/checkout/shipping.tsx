6cefb0fcc6a10578ec5e48174d698fd44d5fccaa
Erfan <erfansormi97@gmail.com>
2023-04-14 23:38:02 +0330

    Update shipping.tsx

--- src/pages/checkout/shipping.tsx
+++ src/pages/checkout/shipping.tsx
@@ -12,22 +12,28 @@ import useCheckout from '../../store/checkout';
 import { BiTimeFive } from 'react-icons/bi';
 
 // components
-import Layout from '../../components/layout/layout'
+import Layout from '../../components/layout/layout';
 import CheckoutHeader from '../../components/checkout/header';
-import CheckoutBox from '../../components/checkout/shipping/checkoutBox';
+import CheckoutBox from '../../components/checkout/checkoutBox';
 import OrderDeliveryAddress from '../../components/checkout/shipping/orderDeliveryAddress';
 import CheckoutSkeleton from '../../components/checkout/checkoutSkeleton';
-import SelectedProducts from '../../components/checkout/shipping/selectedProducts';
+import SelectedProducts from '../../components/checkout/selectedProducts';
 import ChooseShippingTime from '../../components/checkout/shipping/chooseShippingTime';
+import ShippingMethod from '../../components/checkout/shipping/shippingMethod';
 
 const Shipping = () => {
     const { user, loading } = useUserContext();
     const { deliveryHour, deliveryDate } = useCheckout(state => state);
     const date = new DateObject();
+    const router = useRouter();
 
     useEffect(() => {
         if (!user && !loading) {
-            useRouter().push("/auth/login")
+            router.push("/auth/login");
+        }
+
+        if (!user?.cart.products.length || !user.cart.products_counts) {
+            router.push("/");
         }
     }, [user, loading])
 
@@ -39,11 +45,11 @@ const Shipping = () => {
                     user && !loading &&
                     <div className='flex flex-col gap-y-3'>
                         <CheckoutHeader type='shipping' />
-                        <div className='flex flex-col md:flex-row gap-3'>
+                        <div className='flex flex-col-reverse md:flex-row gap-3'>
 
                             {/* checkout box */}
                             <aside className='w-full md:w-2/5 lg:w-[28%] xl:w-1/4'>
-                                <CheckoutBox />
+                                <CheckoutBox type='shipping' />
                             </aside>
 
                             {/* checkout detail */}
@@ -52,12 +58,18 @@ const Shipping = () => {
                                 {/* order delivery address */}
                                 <OrderDeliveryAddress />
 
-                                <div className='border border-solid border-gray-200 rounded-lg py-12 px-6'>
-                                    {/* selected products */}
-                                    <SelectedProducts />
+                                <div className='border border-solid border-gray-200 rounded-lg py-11 px-6'>
+
+                                    <div className='flex flex-col gap-y-12'>
+                                        {/* shipping method */}
+                                        <ShippingMethod />
+
+                                        {/* selected products */}
+                                        <SelectedProducts />
+                                    </div>
 
                                     {/* choose shipping time */}
-                                    <div className='mt-14 flex flex-col gap-y-4'>
+                                    <div className='mt-14 flex flex-col gap-y-4' id="shipping-time">
                                         <div className='flex items-center gap-x-1 text-gray-700 capitalize'>
                                             <span className='flex text-xl'>
                                                 <BiTimeFive />
