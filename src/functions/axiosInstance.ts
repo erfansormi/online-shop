4f6665b2265da058f87bdb3d1ac948ae9b63bf06
Erfan <erfansormi97@gmail.com>
2023-04-19 22:20:02 +0330

    added Bearer token to authorization headers

--- src/functions/axiosInstance.ts
+++ src/functions/axiosInstance.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
+import { getCookie } from 'cookies-next';
 
 export const axiosInstance = axios.create({
     withCredentials: true,
     baseURL: process.env.URL,
     headers: {
         "Content-Type": "application/json",
+        "Authorization": `Bearer ${getCookie('token')}`
     },
 })
